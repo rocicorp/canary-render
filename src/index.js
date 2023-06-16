f4f1e76ad3f51663c8d401768b04fc56b4090813
Gregory Baker <greg@roci.dev>
2023-06-16 14:19:18 -0700

    improve logs, remove _

--- src/index.js
+++ src/index.js
@@ -10,35 +10,44 @@ app.use(
 );
 
 app.get("/canary/get", cors(), function (req, res, next) {
-  console.info("Handling canary get. connectCheckID =", req.query.id);
+  console.info(
+    "/canary/get",
+    "connectCheckID =",
+    req.query.id,
+    "Handling canary get."
+  );
   res.send("hello");
   res.end();
 });
 
 app.ws("/canary/websocket", function (ws, req) {
-  console.info(
-    "Handling canary websocket. connectCheckID =",
-    req.query.id,
-    ", wSecWebSocketProtocolHeader =",
-    req.query.wSecWebSocketProtocolHeader
-  );
-  console.info("Sending hello message");
+  const log = (...args) =>
+    console.info(
+      "/canary/websocket",
+      "connectCheckID =",
+      req.query.id,
+      "wSecWebSocketProtocolHeader =",
+      req.query.wSecWebSocketProtocolHeader,
+      ...args
+    );
+  log("Handling canary websocket.");
+  log("Sending hello message");
   ws.send("hello");
   let closed = false;
   const onClose = () => {
-    console.info("Socket closed.");
+    log("Socket closed.");
     closed = true;
     ws.removeEventListener("close", onClose);
   };
   ws.addEventListener("close", onClose);
   setTimeout(() => {
     if (!closed) {
-      console.info("Closing socket.");
+      log("Closing socket.");
       closed = true;
       ws.removeEventListener("close", onClose);
       ws.close();
     }
-  }, 10_000);
+  }, 10000);
 });
 
 app.listen(3000);
